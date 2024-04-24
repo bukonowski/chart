ce60de160a9e6d66f8bf26b95cefee3dbdf5deb7
Lautaro <lautarollanosnahuel@gmail.com>
2024-04-24 12:53:31 -0300

    termino la api

--- API.py
+++ API.py
@@ -1,52 +1,49 @@
 from kerykeion.charts.kerykeion_chart_svg import KerykeionChartSVG
 from kerykeion.astrological_subject import AstrologicalSubject
 from pathlib import Path
-# AstrologicalSubject
-name = "Rahim"
-year = 1945
-month = 10
-day = 10
-hour = 10
-minute = 10
-city = "Buenos Aires"
-nation = "AR"   #Código de país de dos letras
+from flask import Flask, request, send_file
 
-# KerykeionChartSVG
-style = "dark"
-font = None #String con el nombre de la fuente (debe estar instalada)
-bg_image_wheel = None # string con url de imagen
-bg_image = None #String con url de imagen
-bg_color = None # String con color en cualquier formato admitido por svg
+app = Flask(__name__)
+@app.route('/createSVG', methods=['POST'])
 
-# Variables que no se van a usar pero deben estar presentes
-chart_type = "Natal"
-SecondSubject = None
-output_directory = "output" #Path(__file__).resolve().parent / 'output'
+def generar_archivo():
+    data = request.json
 
+    name = data.get('name')
+    year = data.get('year')
+    month = data.get('month')
+    day = data.get('day')
+    hour = data.get('hour')
+    minute = data.get('minute')
+    city = data.get('city', 'London')
+    nation = data.get('nation', 'GB')
+    style = data.get('style')
+    font = data.get('font', "Belgan Aesthetic")
+    bg_image_wheel = data.get('bg_image_wheel', None)
+    bg_image = data.get('bg_image', None)
+    bg_color = data.get('bg_color', None)
+
+    if style.lower() == "bright":
+        style_path = Path("kerykeion/charts/bright.json")
+    else:
+        style_path = Path("kerykeion/charts/dark.json")
+
+    # create instances
+    subject = AstrologicalSubject(name, year, month, day, hour, minute, city, nation)
+    chart = KerykeionChartSVG(subject, "Natal", None, "output", style_path, font, bg_color, bg_image, bg_image_wheel)
+    chart.makeSVG()
+    svg_file_path = chart.chartname
+    return send_file(svg_file_path,
+                     mimetype='image/svg+xml',
+                     as_attachment=True)
 
 if __name__ == "__main__":
-    from kerykeion.utilities import setup_logging   #solo para debug, se puede borrar
-    setup_logging(level="debug")                    #solo para debug, se puede borrar
-    
-    if style.lower() == "bright":
-        style = Path("kerykeion\\charts\\bright.json")
-    else:
-        style = Path("kerykeion\\charts\\dark.json")
-
-    subject = AstrologicalSubject(name, year, month, day, hour, minute, city)
-    Chart = KerykeionChartSVG(subject, chart_type, SecondSubject, output_directory, style, font, bg_color, bg_image, bg_image_wheel)
-
-    # Generate SVG
-    Chart.makeSVG()
+    #from kerykeion.utilities import setup_logging
+    #setup_logging(level="debug")
+    app.run(debug=False)
 
 
-
-# TODO: Crear API que reciba las variables de arriba (armar el json) (Flask o FastAPI)
 # TODO: Limpiar proyecto¿?
-# TODO: Devolver el chart con la API
-
 # TODO: Subir el proyecto a koyeb.app (Quizá con la cuenta de chambeadevs)
 # TODO: Si usamos la cuenta de chambeadevs hay que hacerle un github
-
-# TODO: Testear API con Postman
 # TODO:subir la colección de postman a rapidapi
